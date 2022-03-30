b68a0605bad80168df3ea554a4420253f700b66d
MCEpicNation <30642012+MCEpicNation@users.noreply.github.com>
2022-03-30 15:15:11 -0400

    Update API.java

--- src/net/twistedmc/twistedpass/Util/API.java
+++ src/net/twistedmc/twistedpass/Util/API.java
@@ -2,11 +2,15 @@ package net.twistedmc.twistedpass.Util;
 
 import net.twistedmc.twistedpass.Main;
 import net.twistedmc.twistedpass.MySQL;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
 
 import static net.twistedmc.twistedpass.Main.season;
 
@@ -37,7 +41,10 @@ public class API {
     }
     // PUBLIC API METHODS \\
     public static boolean UserHasClaimedLevel(Player p,int level) {
-
+        List<String> userLevels = Arrays.asList(getLevelsArray(p));
+        if (userLevels == null) { Bukkit.getLogger().log(Level.SEVERE,"[TwistedPassAPI] Error"); } // Clean :)
+        if (userLevels.contains("" + level)) { return true; }
+        return false;
     }
     public static boolean UserHasProfile(Player p) {
 
